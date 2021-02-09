68adf602e51a695af62c3b672fd28b9e868b2307
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-02-09 05:24:31 -0500

    Delete test code for dynamic values

--- src/App.js
+++ src/App.js
@@ -9,16 +9,7 @@ function App() {
   return (
     <div className="App">
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        {/* <p>{person}</p>  */}
-
-        <p>{10}</p>
-        <p>{"Hello, ninjas"}</p>
-        <p>[1,2,3,4,5]</p>
-        <p>{Math.random(10)}</p>
-
-        <a href={link}>Google Site</a>
+        <h1>App Component</h1>
       </div>
     </div>
   );
