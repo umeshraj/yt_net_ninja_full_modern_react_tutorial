7db66aafaccda19b0a20a1e194d2289283023aea
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-02-10 05:20:39 -0500

    Remove unused button

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,10 @@ const Home = () => {
     console.log(e);
   };
 
-  const handleClickAgain = (e, name) => {
-    console.log("hello " + name);
-    console.log(e);
-  };
-
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click me</button>
-      <button
-        onClick={(e) => {
-          handleClickAgain(e, "Mario");
-        }}
-      >
-        Click me again
-      </button>
     </div>
   );
 };
