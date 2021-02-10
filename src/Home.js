fe8bc56d469424499687e1438c5f86a0910fcd87
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-02-10 05:10:51 -0500

    Click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hello, Ninjas!");
+    console.log(e);
+  };
+
+  const handleClickAgain = (e, name) => {
+    console.log("hello " + name);
+    console.log(e);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain(e, "Mario");
+        }}
+      >
+        Click me again
+      </button>
     </div>
   );
 };
