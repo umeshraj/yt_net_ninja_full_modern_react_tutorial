580bc130349e7a941a7e730e152707c56faa68bc
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-02-10 05:24:57 -0500

    non reactive variable

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,17 @@
+import { useState } from "react";
+
 const Home = () => {
-  const handleClick = (e) => {
-    console.log("Hello, Ninjas!");
-    console.log(e);
+  let name = "mario";
+
+  const handleClick = () => {
+    name = "luigi";
+    console.log(name);
   };
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name}</p>
       <button onClick={handleClick}>Click me</button>
     </div>
   );
