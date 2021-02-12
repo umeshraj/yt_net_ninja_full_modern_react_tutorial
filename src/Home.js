a1c7bd0ab8da186a58586f5325f3fcd42dc3952f
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-02-12 05:52:19 -0500

    Example of useEffect w/ dependency array

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,14 +13,23 @@ const Home = () => {
     },
   ]);
 
+  const [name, setName] = useState("mario");
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
 
+  useEffect(() => {
+    console.log("use effect ran");
+    console.log(name);
+  }, [name]);
+
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName("Luigi")}>Change name</button>
+      <p>{name}</p>
     </div>
   );
 };
