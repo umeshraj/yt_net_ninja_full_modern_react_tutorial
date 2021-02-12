e2bab4c15717a94b32e97a0b9e35e820bcf10f85
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-02-12 05:19:43 -0500

    Passing fns as props to delete blog entry

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,7 @@
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
   //   const blogs = props.blogs;
   //   const title = props.title;
+
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -9,6 +10,7 @@ const BlogList = ({ blogs, title }) => {
           <div className="blog-preview" key={blog.id}>
             <h2>{blog.title}</h2>
             <p>Written by: {blog.author}</p>
+            <button onClick={() => handleDelete(blog.id)}>delete blog</button>
           </div>
         );
       })}
