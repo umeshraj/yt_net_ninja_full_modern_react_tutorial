8d2640e00f138aada6bcce73a347435a2eff1bfb
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2021-02-16 05:22:43 -0500

    Add Link to each blog entry

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title }) => {
   //   const blogs = props.blogs;
   //   const title = props.title;
@@ -8,8 +10,10 @@ const BlogList = ({ blogs, title }) => {
       {blogs.map((blog) => {
         return (
           <div className="blog-preview" key={blog.id}>
-            <h2>{blog.title}</h2>
-            <p>Written by: {blog.author}</p>
+            <Link to={`/blogs/${blog.id}`}>
+              <h2>{blog.title}</h2>
+              <p>Written by: {blog.author}</p>
+            </Link>
           </div>
         );
       })}
